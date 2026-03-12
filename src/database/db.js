b150d60d738df3816a6f9fe14583784bce43a5fe
prwilliam <nwillian533@gmail.com>
2026-03-12 11:13:35 -0300

    Adding environment variables with dotenv...(Adicionando variáveis de ambiente com dotenv)

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 
-const connectDatabase = ()=>{
-    console.log("wait, conecting to the database...(Aguardando conexão com o banco de dados...)");
-    mongoose.connect(
-    "mongodb+srv://root:root@cluster0.i9lydmy.mongodb.net/?appName=Cluster0", 
-     /** Erro no: {useNewUrlParser: true, useUnifiedTopology: true}
-      * Tipo de erro: significa que o driver do MongoDB atual já usa essas opções automaticamente, então não é mais permitido e necessário informá-las.
-      */
-     )
-    .then(() => console.log("MongoDB Atlas connected successfully...(MongoDB Atlas Connectado com sucesso!)"))
-    .catch((err) => console.log(`Error trying to connect to MongoDB Atlas...(Erro ao tentar se conectar com o MongoDB Atlas): ${err}`));
+const connectDatabase = () => {
+  console.log(
+    "wait, conecting to the database...(Aguardando conexão com o banco de dados...)",
+  );
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() =>
+      console.log(
+        "MongoDB Atlas connected successfully...(MongoDB Atlas Connectado com sucesso!)",
+      ),
+    )
+    .catch((err) =>
+      console.log(
+        `Error trying to connect to MongoDB Atlas...(Erro ao tentar se conectar com o MongoDB Atlas): ${err}`,
+      ),
+    );
 };
 
 export default connectDatabase;
